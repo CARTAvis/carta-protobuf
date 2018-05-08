94b2f57a469198a9d5617588e7fab54d680fd4c1
veggiesaurus <accomrie@gmail.com>
2018-05-08 10:39:03 +0200

    initial message and compilation

---
+++ build_proto.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+mkdir -p build
+
+printf "Building all message modules..."
+npx pbjs -t static-module -o build/index.js --wrap es6 control/*.proto request/*.proto stream/*.proto
+npx pbts -o build/index.d.ts build/index.js
+printf "...done\n"
+
+#printf "Building Request message module..."
+#npx pbjs -t static-module -o build/request.js --wrap es6 request/*.proto
+#npx pbts -o build/request.d.ts build/request.js
+#printf "...done\n"
+#
+#printf "Building Stream message module..."
+#npx pbjs -t static-module -o build/stream.js --wrap es6 stream/*.proto
+#npx pbts -o build/stream.d.ts build/stream.js
+#printf "...done\n"
+
+cd ../node_modules
+rm -f carta-protobuf
+ln -s ../protobuf/build carta-protobuf
