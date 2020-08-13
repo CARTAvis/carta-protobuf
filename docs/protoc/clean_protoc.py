52b3f7bcd89c5ca9d0298b3aef92d3d52e4d5de4
Adrianna Pińska <adrianna.pinska@gmail.com>
2020-08-13 21:44:59 +0200

    Output generated protobuf documentation as JSON and parse JSON to rst instead of generating and mangling rst.

--- docs/protoc/clean_protoc.py
+++ docs/protoc/clean_protoc.py
@@ -1,123 +1,155 @@
 #!/usr/bin/env python3
 
 import re
+import json
 import sys
+import glob
+import os
 
-data = {}
+with open(sys.argv[1]) as f:
+    data = json.load(f)
 
-for filename in sys.argv[1:]:
-    with open(filename) as f:
-        data[filename] = f.read()
+map_info = {}
+message_names = set()
 
-# For automatic hyperlinks later
-symbols = set()
+for file_ in data["files"]:
+    for message in file_["messages"]:
+        if message["name"].endswith("Entry"):
+            map_info[message["longName"]] = message
+        else:
+            message_names.add(message["name"])
 
-# Add anchors to all headings
+def to_snake(word):
+    parts = re.findall("[A-Z][a-z]+", word)
+    return "_".join(p.upper() for p in parts)
 
-def fix_heading(m):
-    heading = m.group(1)
-    anchor = heading.lower()
-    if not "." in anchor:
-        symbols.add(heading)
-    underline = "^" * len(heading)
-    return f".. _{anchor}:\n\n{heading}\n{underline}"
+snake_names = set(to_snake(n) for n in message_names)
 
-# Colour some messages red or blue
+rst = {}
 
-def controlcolour(m):
-    fullanchor = m.group(1)
-    anchor = m.group(2)
-    if "." in anchor:
-        return m.group(1)
-    cssclass = "b2f" if anchor.endswith("ack") else "f2b"
-    return f".. cssclass:: {cssclass}\n\n{fullanchor}"
+proto_dir = {os.path.basename(p): os.path.basename(os.path.dirname(p)) for p in glob.glob("../../*/*.proto")}
 
-def requestcolour(m):
-    fullanchor = m.group(1)
-    anchor = m.group(2)
-    if "." in anchor:
-        return m.group(1)
-    cssclass = "b2f" if anchor.endswith("response") else "f2b"
-    return f".. cssclass:: {cssclass}\n\n{fullanchor}"
-
-def streamcolour(m):
-    fullanchor = m.group(1)
-    anchor = m.group(2)
-    if "." in anchor:
-        return m.group(1)
-    cssclass = "f2b" if anchor.endswith("request") else "b2f"
-    return f".. cssclass:: {cssclass}\n\n{fullanchor}"
-
-# Separate map entry to remove and add to the main message table
-
-ENTRY_TABLE = re.compile(r"""
-\.\. _.*\..*entry:
-
-(.*)\.(.*Entry)
-\^+
-
-
-
-
-\.\. list-table::
+for file_ in data["files"]:
+    file_name = file_["name"]
+    output = []
+    
+    for message in file_["messages"]:
+        name = message["name"]
+        # Skip map entries
+        
+        if name.endswith("Entry"):
+            continue
+        
+        # Message heading
+        
+        anchor = name.lower()
+        underline = "^" * len(name)
+        output.append(f".. _{anchor}:\n\n{name}\n{underline}\n")
+        
+        # Link to file on github
+        
+        parent_dir = proto_dir[file_name]
+        output.append(f"Source file: `{file_name} <https://github.com/CARTAvis/carta-protobuf/blob/dev/{parent_dir}/{file_name}>`_\n")
+        
+        # Description
+        
+        description = message["description"]
+        for symbol in message_names | snake_names - set([name, to_snake(name)]):
+            description = re.sub(f"\\b{symbol}\\b", f":carta:`{symbol}`", description)
+        output.append(description)
+        
+        # Fields
+        
+        if message["hasFields"]:
+        
+            field_header = """
+.. list-table::
    :widths: 25 25 10 40
    :header-rows: 1
 
-   \* - Field
+   * - Field
      - Type
      - Label
-     - Description
-   \* - key
-     - (.*)
-     - 
-     - 
-   \* - value
-     - (.*)
-     - 
-     - 
-""")
-
-map_info = {}
-
-for filename in data:
-    data[filename] = re.sub("\n(.*?)\n\^{5,}", fix_heading, data[filename])
-    
-    if "control" in filename:
-        data[filename] = re.sub("(\.\. _(.*):)", controlcolour, data[filename])
-    elif "request" in filename:
-        data[filename] = re.sub("(\.\. _(.*):)", requestcolour, data[filename])
-    elif "stream" in filename:
-        data[filename] = re.sub("(\.\. _(.*):)", streamcolour, data[filename])
-    
-    # Gather the map info and delete the map entries in the first pass
-    for msgname, mapname, keytype, valuetype in  ENTRY_TABLE.findall(data[filename]):
-        map_info[(msgname, mapname)] = (keytype, valuetype)
-    
-    data[filename] = ENTRY_TABLE.sub("", data[filename])
-    
-# Second pass to rewrite the maps in the main tables
-
-for filename in data:
-    for (msgname, mapname), (keytype, valuetype) in map_info.items():
-        data[filename] = re.sub(f"({msgname}\n\^+.*?- ){mapname}", f"\\1map<key: {keytype}, value: {valuetype}>", data[filename], flags=re.DOTALL)
+     - Description"""
+            
+            output.append(field_header)
+            
+            for field in message["fields"]:
+                field_name = field["name"]
+                
+                output.append(f"   * - {field_name}")
+                
+                if field["ismap"]:
+                    map_message = map_info[f"{name}.{field['type']}"]
+                    map_key = map_message["fields"][0]["type"]
+                    map_value = map_message["fields"][1]["type"]
+                    if map_value in message_names:
+                        map_value = f":carta:`{map_value}`"
+                    field_type = f"map<key: {map_key}, value: {map_value}>"
+                elif field["type"] in message_names:
+                    field_type = f":carta:`{field['type']}`"
+                else:
+                    field_type = field["type"]
+                
+                output.append(f"     - {field_type}")
+                output.append(f"     - {field['label']}")
+                
+                field_desc = field["description"]
+                field_desc = field_desc.replace("\n", " ")
+                for symbol in message_names | snake_names - set([name, to_snake(name)]):
+                    field_desc = re.sub(f"\\b{symbol}\\b", f":carta:`{symbol}`", field_desc)
+                
+                output.append(f"     - {field_desc}")
         
-# Third pass to add automatic :carta: hyperlinks to all messages
-symbols_lower = set(s.lower() for s in symbols)
-
-def auto_snake_link(m):
-    name = m.group(1)
-    if name.lower().replace("_", "") in symbols_lower:
-        return f":carta:`{name}`"
-    return m.group(0)
-
-for filename in data:
-    # Automatic links for CamelCaseNames not at the start of a line
-    for symbol in symbols:
-        data[filename] = re.sub(f"(?<!^)\\b{symbol}\\b", f":carta:`{symbol}`", data[filename], flags=re.MULTILINE)
+            output.append("")
         
-    # Automatic links for recognised SNAKE_CASE_NAMES not at the start of a line
-    data[filename] = re.sub(r"(?<!^)\b([A-Z_]{3,})\b", auto_snake_link, data[filename], flags=re.MULTILINE)
+    for enum in file_["enums"]:
+        name = enum["name"]
+        
+        # Enum heading
+        
+        anchor = name.lower()
+        underline = "^" * len(name)
+        output.append(f".. _{anchor}:\n\n{name}\n{underline}\n")
+        
+        # Description
+        
+        description = enum["description"]
+        for symbol in message_names | snake_names - set([name, to_snake(name)]):
+            description = re.sub(f"\\b{symbol}\\b", f":carta:`{symbol}`", description)
+        output.append(description)
+        
+        # Values
+                
+        val_header = """
+.. list-table::
+   :widths: 33 33 33
+   :header-rows: 1
 
-for filename, contents in data.items():
-    with open(filename, "w") as f:
-        f.write(contents)
+   * - Name
+     - Number
+     - Description"""
+        
+        output.append(val_header)
+        
+        for val in enum["values"]:
+            output.append(f"   * - {val['name']}")
+            output.append(f"     - {val['number']}")
+            output.append(f"     - {val['description']}")
+        
+        output.append("")
+        
+    rst[file_name] = "\n".join(output)
+
+output_map = {
+    "control": [os.path.basename(p) for p in glob.glob("../../control/*.proto")],
+    "request": [os.path.basename(p) for p in glob.glob("../../request/*.proto")],
+    "stream": [os.path.basename(p) for p in glob.glob("../../stream/*.proto")],
+    "defs": ["defs.proto"],
+    "enums": ["enums.proto"],
+}
+
+for section, filenames in output_map.items():
+    with open(f"../{section}.rst.txt", "w") as f:
+        for filename in sorted(filenames):
+            print(rst[filename], file=f)
