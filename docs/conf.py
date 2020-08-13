c299c9ea376c34e695210b53a4db659457af75da
Adrianna Pińska <adrianna.pinska@gmail.com>
2020-08-13 04:16:33 +0200

    made colours work

--- docs/conf.py
+++ docs/conf.py
@@ -52,3 +52,15 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+]
+
+rst_prolog = """
+.. role:: comment
+
+.. role:: f2b
+
+.. role:: b2f
+"""
